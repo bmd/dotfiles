f10f7409646c70bf83fd30d3c45998d934b269f9
Brendan Maione-Downing <b.maionedowning@gmail.com>
2019-05-04 13:52:01 -0400

    Documentation and cleanup

--- dotfiles/aws.zsh
+++ dotfiles/aws.zsh
@@ -24,12 +24,25 @@ aws::acm::lookup() {
       --certificate-arn arn:aws:acm:us-east-1:${account}:certificate/${certificate}
 }
 
+# Use AWS's SessionManager to open a remote pseudo-shell on a
+# random instance within an Elastic Beanstalk environment. This
+# saves the cumbersome process of mapping the instance ID required
+# by SessionManager to the currently live instances within the
+# EB app.
+#
+# Usage:
+#   aws::beanstalk::session::start environment [profile]
 aws::beanstalk::session::start() {
     local environment profile instance
 
+    if [[ -z "$1" ]]; then
+        echo "Usage: aws::beanstalk::session::start environment [profile]"
+        return
+    fi
     environment="$1"
     [[ -n $2 ]] && profile="$2" || profile="default"
-    local instance=$(aws elasticbeanstalk describe-environment-resources --profile=${profile} --environment-name=${environment} | jq -r ".EnvironmentResources.Instances[].Id" | sort -R | head -n 1)
+
+    instance=$(aws elasticbeanstalk describe-environment-resources --profile=${profile} --environment-name=${environment} | jq -r ".EnvironmentResources.Instances[].Id" | sort -R | head -n 1)
 
     aws ssm start-session --profile=${profile} --target ${instance}
 }
@@ -37,7 +50,7 @@ aws::beanstalk::session::start() {
 aws::ecr::repo::size() {
     local repository profile size
 
-    if [[ -n $1 ]]; then
+    if [[ -z $1 ]]; then
         echo "Usage: $0 repository [profile]"
         return
     fi
@@ -45,16 +58,22 @@ aws::ecr::repo::size() {
     repository="$1"
     profile=$(aws::profile::resolve $2)
 
-    size=$(aws ecr describe-images --repository-name ${repository} --profile ${profile} | jq '.imageDetails | map(.imageSizeInBytes) | add | ((. // 0) / (1024 * 1024)) | floor')
+    size=$(aws ecr describe-images --profile=${profile} --repository-name ${repository} | jq '.imageDetails | map(.imageSizeInBytes) | add | ((. // 0) / (1024 * 1024)) | floor')
     echo "${repository}: ${size}mb"
 }
 
+# Resolve the AWS profile to pass to the --profile flag of AWS CLI
+# commands. The order of resolution is:
+#   1) An explicit argument passed to this function
+#   2) The value of the AWS_DEFAULT_PROFILE environment variable
+#   3) "default"
+#
+# Usage:
+#   aws::profile::resolve [profile]
 aws::profile::resolve() {
     local profile
     profile="${AWS_DEFAULT_PROFILE:-default}"
-    [[ -n $1 ]] && profile="$1"
+    [[ -n "$1" ]] && profile="$1"
 
-    return profile
+    echo "${profile}"
 }
-
-
