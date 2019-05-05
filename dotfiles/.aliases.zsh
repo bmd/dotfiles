dc27d84e4e3dff14c394332b564f5ee2a1d4f4da
Brendan Maione-Downing <b.maionedowning@gmail.com>
2019-05-05 10:46:04 -0400

    Get tesseract_session command working

--- dotfiles/.aliases.zsh
+++ dotfiles/.aliases.zsh
@@ -24,4 +24,25 @@ restore_ssh_keys() { op::keys::restore $HOME/.ssh "Blue State Digital" "ssh-keys
 
 # Tesseract helpers
 tesseract_cert() { aws::acm::lookup 093597997342 $1 us-east-1 bsd-tesseract | jq . ; }
-tesseract_session() { aws::beanstalk::session $1 ; }
+
+# We need to bridge the way we identify environments in tesseract (e.g. "auth-int") with
+# how our Beanstalk environments are named.
+#
+# Usage:
+#   tesseract_session environment
+# Example:
+#   tesseract_session auth-int
+tesseract_session() {
+    local name parts beanstalk
+
+    name="$1"
+    parts=("${(@s/-/)name}")
+    beanstalk=$(
+        aws resourcegroupstaggingapi get-resources \
+            --resource-type-filters elasticbeanstalk \
+            --tag-filters Key=service,Values=${parts[1]} Key=environment,Values=${parts[2]} \
+            | jq -r '.ResourceTagMappingList[0].Tags[] | select(.Key == "Name").Value'
+    )
+
+    aws::beanstalk::session::start ${beanstalk}
+}
