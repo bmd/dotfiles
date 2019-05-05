7edd8d324457f1a23ff6ad4e734f73c3540f555a
Brendan Maione-Downing <b.maionedowning@gmail.com>
2019-05-05 13:12:24 -0400

    More docs

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ These are my dotfiles. There are many like them, but these ones are mine.
 
 ## Installing on a fresh machine
 
+Run
+
+```bash
+make install
+```
+
+This will run the bootstrap script as well as installing the symlinks
+
 ## Refreshing symlinks
 
 Just run
--- dotfiles/.aliases.zsh
+++ dotfiles/.aliases.zsh
@@ -1,12 +1,18 @@
 #!/bin/zsh
 # .aliases.zsh
 
-# Simple command aliases
+# ----------------------------------------
+# Commands
+# ----------------------------------------
+
 alias bt='ssh bastion-tools'
 alias mkdir="mkdir -p"
 alias reload="source ~/.zshrc"
 
-# Include my shell libraries
+# ----------------------------------------
+# Libraries
+# ----------------------------------------
+
 source $HOME/.aws.zsh
 source $HOME/.helpers.zsh
 
@@ -29,7 +35,7 @@ clone() {
 # Usage:
 #   err message
 # Example:
-#   error "Your function call is bad and you should feel bad"
+#   err "Your function call is bad and you should feel bad"
 err() {
     echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
 }
@@ -42,6 +48,18 @@ j64() {
     echo $@ | base64 --decode | jq .
 }
 
+# Use JQ with YAML data by converting the YAML to JSON on the fly.
+# NB: this will probably perform horribly with large objects, it's just
+# meant as a convenience method, not for heavy lifting. You can also
+# pass through most JQ options here, although it hasn't been exhaustively
+# tested.
+#
+# Usage:
+#   yq data [...options]
+yq() {
+    yaml2json | jq $@
+}
+
 # Create a new directory and change into it
 #
 # Usage:
@@ -50,12 +68,33 @@ mcd() {
     mkdir -p "$1" && cd "$1"
 }
 
-monitor_url() { curl::loop $@ ; }
-yq() { yaml2json | jq $@ ; }
+# Monitor a URL at a defined interval and print out a templated string.
+# The format string is passed to `curl -w`.
+#
+# Usage:
+#   monitor_url url interval fmt
+# Examples:
+#   monitor_url https://google.com
+#   monitor_url https://google.com 5 "The status code is: %{http_code}"
+monitor_url() {
+    curl::loop $@
+}
 
-# SSH Keys helpers
-backup_ssh_keys() { op::keys::backup $HOME/.ssh "Blue State Digital" "ssh-keys"; }
-restore_ssh_keys() { op::keys::restore $HOME/.ssh "Blue State Digital" "ssh-keys" ; }
+# Backup my SSH keys to a 1Password vault using the op CLI tool.
+#
+# Usage:
+#   backup_ssh_keys
+backup_ssh_keys() {
+    op::keys::backup $HOME/.ssh "Blue State Digital" "ssh-keys"
+}
+
+# Restore my backed-up SSH keys from a 1Password vault
+#
+# Usage:
+#   restore_ssh_keys
+restore_ssh_keys() {
+    op::keys::restore $HOME/.ssh "Blue State Digital" "ssh-keys"
+}
 
 # Tesseract helpers
 tesseract_cert() { aws::acm::lookup 093597997342 $1 us-east-1 bsd-tesseract | jq . ; }
